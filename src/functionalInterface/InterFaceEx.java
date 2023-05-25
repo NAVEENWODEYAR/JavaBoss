6ce7781d33e040e3f1db65694e4c2d35e090f784
Naveen <naveenkrishnawodeyar@gmail.com>
2023-05-25 17:17:28 +0530

    first commit

---
+++ src/functionalInterface/InterFaceEx.java
@@ -0,0 +1,12 @@
+package functionalInterface;
+
+public interface InterFaceEx
+{
+    public static void main(String args[])
+    {
+        System.out.println("We can run the code without the class!");
+            for (;;)
+            {
+            }
+    }
+}
