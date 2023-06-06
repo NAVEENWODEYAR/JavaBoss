1cc0cfa2ebde89ad550b4d43a78ef4d1512f317d
Naveen <naveenkrishnawodeyar@gmail.com>
2023-06-06 10:06:21 +0530

    gm

---
+++ src/dateTimeAPI/Leap_Ex.java
@@ -0,0 +1,12 @@
+// Java program to check leap year
+package dateTimeAPI;
+
+import java.time.LocalDate;
+public class Leap_Ex
+{
+    public static void main(String[] args)
+    {
+    //  dateTime API
+        System.out.println(LocalDate.now().isLeapYear());
+        }
+}
